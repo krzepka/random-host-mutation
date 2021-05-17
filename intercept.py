6181644dc0f32b65576f4b5dc914856d8844b931
JakubDmochowski <35743552+JakubDmochowski@users.noreply.github.com>
2021-05-17 17:09:07 +0200

    Update intercept.py

--- intercept.py
+++ intercept.py
@@ -1,33 +1,33 @@
-import logging
-
 from scapy.all import sniff
-from scapy.interfaces import get_if_list
+from scapy.all import sendp
+from scapy.all import bridge_and_sniff
 
-iface_in = "eth0"
+from netaddr import IPNetwork
 
-mac_dst = ""
-ip_dst = "192.168.1.1"
+def generate_virtual_address_from_range(_range):
+    # TODO: handle vAddr generation
+    return _range
 
+def encode_packet(pkt):
+    # TODO: handle packet encoding
+    new_pkt = pkt.clone()
+    if rAddr_to_vAddr contains rAddr:
+        new_pkt.dst = rAddr_to_vAddr[pkt.dst]
+    else
+        rAddr_to_vAddr[pkt.dst] = generate_vAddr(pkt.dst)
+        new_pkt.dst = rAddr_to_vAddr[pkt.dst]
+    return new_pkt
 
-def handle_packet_in(pkt):
-    pkt.show()
-
-    # packet = pkt.copy()
-    # packet.dst = ip_dst
-    #
-    # sendp(pkt, iface=iface_out)
-
+def decode_packet(pkt):
+    # TODO: handle packet decoding
+    new_pkt = pkt.clone()
+    new_pkt.dst = vAddr_to_rAddr[pkt.dst]
+    return new_pkt if new_pkt.dst else False
 
 def main():
-    if_list = get_if_list()
-    print(if_list)
-
-    # AsyncSniffer(iface=iface_in, prn=handle_packet_in, store=0)
-    sniff(iface=iface_in, prn=handle_packet_in, store=0)
-
-    # sniffer_eth0.start()
-
+    bridge_and_sniff(if1="eth0", if2="eth1", xfrm12=decode_packet, xfrm21=encode_packet)
 
 if __name__ == "__main__":
     main()
+
 
