4f679f889899bfdc9303b32e04f12b9725b13a67
Jakub Dmochowski <jakub@dmochowski.co>
2021-05-24 16:43:21 +0200

    MTC http communication

--- MTC.py
+++ MTC.py
@@ -2,7 +2,10 @@ import os
 import random
 import time
 import logging
+import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from netaddr import *
 
 from scapy.all import sniff
 from scapy.layers.http import *
@@ -43,21 +46,29 @@ address space: (2, 254)
 def flatten_list_of_lists(list_of_lists):
     return [element for lst in list_of_lists for element in lst]
 
+def addresses_to_string(ip_set):
+    return [str(addr) for addr in ip_set]
 
 class MTC:
     def __init__(self, shared_key, LFM_interval=900, default_host_space_requirement=1,
                  default_host_mutation_interval=10):
         self.shared_key = shared_key
         self.LFM_interval = LFM_interval
+        self.last_LFM_timestamp = time.time()
         self.default_host_space_requirement = default_host_space_requirement
         self.default_host_mutation_interval = default_host_mutation_interval
 
-        self.hosts = [3]
+        self.hosts = []
         self.host_space_requirement = {3: 2}  # minimal number of addresses for host
         self.host_mutation_interval = {}  # mutation interval
-        self.address_space = [i for i in range(2, 255)]  # single subnet has addresses from 2 to 255
-        self.assigned_addresses = {3: None}  # active sessions, starting with single rIP=192.168.1.3
-        self.assigned_ranges = {3: []}  # assigned VAR's
+        self.address_space = IPSet(IPRange('192.168.1.2', '192.168.1.254'))
+        self.assigned_addresses = {}  # active sessions, starting with single rIP=192.168.1.3
+        self.assigned_ranges = {}  # assigned VAR's
+        self.mutation_index = 0
+        self.init_LFM_schedule()
+
+        self.add_host('192.168.1.2')
+        self.add_host('192.168.1.3')
 
     def mask_addresses_out(self, used_addresses):
         # create sets of contiguous addresses by masking used addresses out from the address space
@@ -99,29 +110,40 @@ class MTC:
             raise Exception("Address space too small")
 
         new_range = random.sample(flattened_available_addresses, range_size)
-        self.assigned_ranges[rIP] = new_range
+        self.assigned_ranges[str(rIP)] = new_range
+        logging.debug("assigning new range to host: " + rIP)
+        logging.debug(addresses_to_string(self.assigned_ranges[str(rIP)]))
 
-        logging.debug(f"Assigned new VAR: {new_range}, for rIP: {rIP}")
         return new_range
 
-    def raise_incorrect_assigned_range_access(self, rIP):
-        if rIP not in self.assigned_ranges:
-            raise Exception(f"Tried to access nonexistent host address: {rIP}")
-
     def get_host_address_range(self, rIP):
-        self.raise_incorrect_assigned_range_access(rIP)
+        if rIP not in self.hosts:
+            raise Exception(f"Host does not exist: {rIP}")
 
-        if not self.assigned_ranges[rIP]:
+        if rIP not in self.assigned_ranges.keys():
+            logging.debug("assigning new address range for host: " + str(rIP))
             self.assign_new_address_range(rIP)
 
-        logging.debug(f"Returning VAR for rIP {rIP}: {self.assigned_ranges[rIP]}")
         return self.assigned_ranges[rIP]
 
     def low_frequency_mutation(self):
         logging.debug("LFM invoked")
+        self.mutation_index += 1
         for host_ip in self.hosts:
             self.assign_new_address_range(rIP=host_ip)
             # TODO: LFM constraints
+
+    def handle_time_check(self):
+        now = time.time()
+        if int(self.last_LFM_timestamp - now) > 2 * self.LFM_interval:
+            schedule.cancel_all()
+            self.init_LFM_schedule()
+        else:
+            schedule.run_pending()
+
+    def init_LFM_schedule(self):
+        self.mutation_index = 0
+        schedule.every(self.LFM_interval).seconds.do(self.low_frequency_mutation)
 
     def add_host(self, rIP, space_requirement=None, mutation_interval=None):
         if space_requirement is None:
@@ -132,45 +154,66 @@ class MTC:
         self.host_space_requirement[rIP] = space_requirement
         self.host_mutation_interval[rIP] = mutation_interval
 
-    def handle_shared_key_request(self, pkt):
+    def handle_shared_key_request(self):
+        return str(self.shared_key)
+
+    def handle_authorization_request(self, data):
         # TODO: implement
         pass
 
-    def handle_authorization_request(self, pkt):
-        # TODO: implement
-        pass
+    def handle_mutation_index_request(self):
+        return str(self.mutation_index)
 
-    def handle_mutation_index_request(self, pkt):
-        # TODO: implement
-        pass
+    def handle_virtual_address_ranges_request(self, rIP):
+        var = self.get_host_address_range(rIP)
+        return json.dumps(addresses_to_string(var))
 
-    def handle_virtual_address_ranges_request(self, pkt):
-        # TODO: implement
-        pass
+logging.basicConfig(level=logging.DEBUG)
+load_dotenv(dotenv_path=join(dirname(__file__), ".env"))
 
-    def handle_http_request(self, pkt):
-        pass
+lfm_interval = int(os.environ["LFM_INTERVAL"])  # in seconds
+shrd_key = os.environ["SHARED_KEY"]
 
-    def handle_packet(self, pkt):
-        pkt.summary()
-        schedule.run_pending()
-
-        if pkt:
-            self.handle_http_request(pkt)
-
+mtc = MTC(shared_key=shrd_key, LFM_interval=lfm_interval)
 
 class MTCRequestHandler(BaseHTTPRequestHandler):
+    def __init__(self, *args):
+        self.map_type_to_GET_request = {
+            'key': mtc.handle_shared_key_request,
+            'm_index': mtc.handle_mutation_index_request,
+            'var': mtc.handle_virtual_address_ranges_request,
+        }
+        super().__init__(*args)
+
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-        self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        mtc.handle_time_check()
+        query = parse_qs(urlparse(self.path).query)
+        logging.info("POST request,\nrequestParams: %s", query)
+        request = {
+            key: value[0] for key, value in query.items()
+        }
+        r_args = []
+        if request['type'] == 'var':
+            r_args.append(request['rIP'])
+        if request['type'] in self.map_type_to_GET_request.keys():
+            try:
+                response = self.map_type_to_GET_request[request['type']](*r_args)
+                self._set_response()
+                self.wfile.write(response.encode('utf-8'))
+                return
+            except Exception as inst:
+                self.send_error(400, message=str(inst))
+                return
+        
+        self.send_error(400, message="Wrong request type")
 
     def do_POST(self):
+        mtc.handle_time_check()
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
@@ -180,27 +223,22 @@ class MTCRequestHandler(BaseHTTPRequestHandler):
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
 
 
-def run_http_server(server_class=HTTPServer, ip='', port=8080, handler_class=MTCRequestHandler):
+def run_http_server(server_class=HTTPServer,
+                    ip='',
+                    port=8080,
+                    handler_class=MTCRequestHandler):
     server_address = (ip, port)
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting MTG HTTP server\n')
+    logging.info('Starting MTC HTTP server on address ' + str(ip) + ' port ' + str(port) + '\n')
 
     httpd.serve_forever()
     httpd.server_close()
-    logging.info('Stopping MTG HTTP server\n')
+    logging.info('Stopping MTC HTTP server\n')
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    load_dotenv(dotenv_path=join(dirname(__file__), ".env"))
-    lfm_interval = int(os.environ["LFM_INTERVAL"])  # in seconds
-    shrd_key = os.environ["SHARED_KEY"]
 
-    # mtc = MTC(shared_key=shrd_key, LFM_interval=lfm_interval)
-    # schedule.every(lfm_interval).seconds.do(mtc.low_frequency_mutation)
-    # sniff(session=TCPSession, iface="Ethernet", prn=mtc.handle_packet)
-
-    run_http_server(ip='0.0.0.0')
+    run_http_server(ip='127.0.0.1')
 
 
 if __name__ == "__main__":
